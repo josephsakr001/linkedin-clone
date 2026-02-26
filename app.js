82c97e5fabcc68d63d0315a61c060cc63441322f
joseph <jsakr212005@gmail.com>
2026-02-26 09:22:04 +0200

    Fix Supabase URL

---
+++ app.js
@@ -0,0 +1,5 @@
+// 1) Connect to Supabase (fill these in)
+const SUPABASE_URL = "https://fknmufaymoefcvljnitu.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_rETTErV0GMfPB-xM73nDWw_777TD36v";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
